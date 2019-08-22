3eabf63079c16cf6408415799e74f56b77cd3ad8
Carl-Petter Bertell <carl-petter.bertell@klarna.com>
2019-08-22 23:28:30 +0200

    support for flagging and difficulty settings

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,35 @@
 <script>
-import Board from './Board.svelte';
+  import Board from "./Board.svelte";
 
-export let name;
+  export let name;
 
-let height = 8;
-let width = 6;
-let mines = 10;
+  let height = 8;
+  let width = 6;
+  let mines = 10;
+
+  function update(h, w, m) {
+    height = h;
+    width = w;
+    mines = m;
+  }
 </script>
 
 <style>
-h1 {
-  color: purple;
-  text-align: center;
-}
+  h1 {
+    color: purple;
+    text-align: center;
+  }
+  .settings {
+    text-align: center;
+  }
 </style>
 
 <div>
   <h1>Sveltesweeper</h1>
+  <div class="settings">
+    <button on:click={() => update(8, 6, 10)}>easy</button>
+    <button on:click={() => update(14, 20, 60)}>medium</button>
+    <button on:click={() => update(20, 40, 200)}>hard</button>
+  </div>
   <Board {height} {width} {mines} />
 </div>
--- src/Cell.svelte
+++ src/Cell.svelte
@@ -1,28 +1,53 @@
 <script>
-export let cell;
-export let onReveal;
-
+  export let cell;
+  export let onReveal;
+  export let onFlag;
 </script>
+
 <style>
-.cell {
-  display: block;
-  width: 20px;
-  height: 20px;
-  line-height: 20px;
-  border: 1px solid #aaa;
-  text-align: center;
-  background-color: #e0e0e0;
-}
-.revealed {
-  background-color: transparent;
-}
-.mine.revealed {
-  background-color:#ff6969;
-}
+  .cell {
+    display: block;
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    border: 1px solid #aaa;
+    text-align: center;
+    background-color: #e0e0e0;
+    cursor: pointer;
+  }
+  .cell:hover {
+    background-color: #ededed;
+  }
+  .revealed {
+    background-color: transparent;
+    cursor: default;
+  }
+  .revealed:hover {
+    background-color: transparent;
+  }
+  .mine.revealed {
+    background-color: #ff6969;
+  }
 </style>
 
-<div class="cell { cell.isMine ? 'mine' : ''} { cell.isRevealed ? 'revealed' : ''}" on:click={() => onReveal(cell.idx)}>
-{#if cell.isMine && cell.isRevealed}*
-{:else if cell.neighbouringMinesCnt > 0 && cell.isRevealed}{cell.neighbouringMinesCnt}
-{/if} 
+<div
+  class="cell {cell.isMine ? 'mine' : ''}
+  {cell.isRevealed ? 'revealed' : ''}"
+  on:click={e => {
+    onReveal(cell.idx);
+  }}
+  on:mousedown={e => {
+    e.stopPropagation();
+    if (e.button === 2) {
+      onFlag(cell.idx);
+    }
+    return false;
+  }}>
+  {#if cell.isMine && cell.isRevealed}
+    ✴
+  {:else if cell.isFlagged}
+    ⚑
+  {:else if cell.neighbouringMinesCnt > 0 && cell.isRevealed}
+    {cell.neighbouringMinesCnt}
+  {/if}
 </div>
